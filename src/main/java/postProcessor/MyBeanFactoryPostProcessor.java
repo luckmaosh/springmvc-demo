81389405d617973d9155f47f9a758c2b6d2ead48
毛少辉 <shaohuimao@sohu-inc.com>
2015-08-04 20:44:41 +0800

    postProcessor

---
+++ src/main/java/postProcessor/MyBeanFactoryPostProcessor.java
@@ -0,0 +1,24 @@
+package postProcessor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory)
+            throws BeansException {
+        // TODO Auto-generated method stub
+        //BeanFactoryPostProcessor可以修改BEAN的配置信息而BeanPostProcessor不能
+        //我们在这里修改postProcessorBean的username注入属性
+        BeanDefinition bd = beanFactory.getBeanDefinition("postProcessorBean");
+        MutablePropertyValues pv = bd.getPropertyValues();
+        if (pv.contains("username")) {
+            pv.addPropertyValue("username", "xiaojun");
+        }
+
+    }
+
+}
---
+++ src/main/java/postProcessor/MyBeanPostProcessorTest.java
@@ -0,0 +1,18 @@
+package postProcessor;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by maoshaohui [maoshh88@gmail.com]
+ * on 15/8/3.上午9:44
+ */
+public class MyBeanPostProcessorTest extends TestCase {
+
+    public void testPostProcessAfterInitialization() throws Exception {
+
+    }
+
+    public void testPostProcessBeforeInitialization() throws Exception {
+
+    }
+}
