0d6abd533aa51c5ec253feda0a4d3e9fdf3222e3
毛少辉 <shaohuimao@sohu-inc.com>
2015-08-13 11:49:30 +0800

    mmmm

--- src/main/java/org/format/demo/controller/IndexController.java
+++ src/main/java/org/format/demo/controller/IndexController.java
@@ -1,18 +1,13 @@
 package org.format.demo.controller;
 
-import com.niux.spring.jedis.JedisExecutor;
-import com.niux.spring.jedis.JedisOperation;
-import com.niux.spring.jedis.OperationResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import redis.clients.jedis.ShardedJedis;
 
 import java.util.Set;
 
@@ -24,9 +19,6 @@ public class IndexController {
     public static Logger logger = LoggerFactory.getLogger(IndexController.class);
 
 
-    @Autowired
-    private JedisExecutor jedisExecutor;
-
     @RequestMapping
     public ModelAndView index(@RequestHeader HttpHeaders headers) {
         logger.info("from request:" + headers.getFirst("LOGID"));
@@ -35,19 +27,6 @@ public class IndexController {
             for (String key : keys) {
                 System.out.println("from request " + key + "=" + headers.getFirst(key));
             }
-        }
-
-
-        try {
-            Object kkk = jedisExecutor.execute(new JedisOperation() {
-                @Override
-                public <T> OperationResult<T> doJedis() {
-                    ShardedJedis shardedJedis = jedisExecutor.getShardedJedis();
-                    return new OperationResult(shardedJedis, shardedJedis.exists("kkk"));
-                }
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
         }
 
         //找到index对应的视图，在WEB-INF/view下
