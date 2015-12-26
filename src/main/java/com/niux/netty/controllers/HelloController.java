0ed59076ea2943c93fd7ab18508a0fa466720d7a
shaohuimao <shaohuimao@sohu-inc.com>
2015-12-26 16:32:32 +0800

    simple

---
+++ src/main/java/com/niux/netty/controllers/HelloController.java
@@ -0,0 +1,29 @@
+package com.niux.netty.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by shaohui.mao
+ * on 15/12/26.下午2:50
+ */
+@RequestMapping("/")
+@org.springframework.stereotype.Controller
+public class HelloController {
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String index() {
+        return "hello";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView nothing() {
+        logger.info("nothing");
+        ModelAndView mv = new ModelAndView("hello");
+        return mv;
+    }
+}
---
+++ src/main/java/com/niux/netty/filter/HelloFilter.java
@@ -0,0 +1,24 @@
+package com.niux.netty.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by shaohui.mao
+ * on 15/12/26.下午2:48
+ */
+public class HelloFilter implements Filter {
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("init filter ...");
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println("hello filter");
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    public void destroy() {
+        System.out.println("destroy filter");
+    }
+}
